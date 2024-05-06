b1e7f587e283586728961f19060ad842f0efc990
vvolf02 <160361495+vvolf02@users.noreply.github.com>
2024-05-05 23:04:11 -0500

    commit 2
    este commit tiene la version terminada (pendiente por corroborar su correcto funcionamiento) de las funciones enfocadas a las solicitudes del usuario

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 
 using namespace std;
+string resize(string row, char caracter);
+string grupito(string row, char caracter);
+char pedirLinea(string lineas[], int tamano);
+bool encontrarEstacion(string row, string estacion, string* posicion);
 
 //para contar el numero de lineas, solo seria contar cuantos elementos haya en el arreglo o lugar en el que se guarden los objetos (pendiente por determinar)
 
@@ -19,7 +24,7 @@ int numEstaciones(string linea, char coma)//se ingresa el string con todos los d
 bool estacionLinea(string lineas[])//busca determinar si la estacion se encuentra en la linea ingresada
 {
     char linea;
-    string estacion, estacion2, lineaComp, linea2, linea3;
+    string estacion, estacion2, lineaComp, linea2, linea3, pos;
     bool valid=true;
     int tamTotal=sizeof(lineas);
     int tamFilas=sizeof(lineas[0]);
@@ -40,34 +45,110 @@ bool estacionLinea(string lineas[])//busca determinar si la estacion se encuentr
         {
             cout<<"La linea ingresada no se encuentra registrada"<<endl;
         }
-    }   
+    }
+    cout<<"Ingrese el nombre de la estación que desea comprobar"<<endl;
+    cin>>estacion;
+    valid=encontrarEstacion(lineaComp, estacion, &pos);
+    return valid;
+}
+
+int tiempoEstaciones(string lineas[])//determina el tiempo que toma ir de una estacion a otra. Puede colocarse directamente en el main o dejarse como funcion o metodo
+{
+    bool valid=false;
+    char linea;
+    int tiempo=0;
+    int tamTotal=sizeof(lineas);
+    int tamFilas=sizeof(lineas[0]);
+    int tamano=tamTotal/tamFilas;
+    string estacion1, estacion2, row, posicion1, posicion2, linea1, linea2;
+    linea=pedirLinea(lineas, tamano);
+    for(int i=0; i<tamano; i++)
+    {
+        if(lineas[i][0]==linea)
+        {
+            row=lineas[i][1];
+        }
+    }
     while(!valid)
     {
-        cout<<"Ingrese el nombre de la estación que desea comprobar"<<endl;
-        cin>>estacion;
-        linea2=resize(lineaComp, ':');
-        linea3=grupito(linea2, '-');
-        linea3=resize(linea3, ' ');
-        if(linea3==estacion)
-        valid=true;
-        linea2=resize(linea2, ',');
-        if(linea2.size()==0)
-        valid=true;
+        cout<<"Ingrese la estacion de salida"<<endl;
+        cin>>estacion1;
+        valid=encontrarEstacion(row, estacion1, &posicion1);
+        if(!valid)
+        cout<<"La estacion ingresada no se encuentra registrada en la linea "<<linea<<endl;
     }
-    if(linea3==estacion)
-    valid=true;
-    return valid;//retorna true si la estacion si se encuentra en la linea
-}
+    valid=false;
+    while(!valid)
+    {
+        cout<<"Ingrese la estacion de llegada"<<endl;
+        cin>>estacion2;
+        valid=encontrarEstacion(row, estacion2, &posicion2);
+        if(!valid)
+        cout<<"La estacion ingresada no se encuentra registrada en la linea "<<linea<<endl;
+        if(posicion2==posicion1)
+        {
+            cout<<"La estacion ingresada es la misma a la ingresada previamente..."<<endl;
+            valid=false;
+        }
 
-float tiempoEstaciones(string lineas[])//determina el tiempo que toma ir de una estacion a otra
-{
-    char linea;
-    string estacion1, estacion2;
-    linea=pedirLinea(lineas);
-    cout<<"Ingrese la estacion de salida"<<endl;
-    cin>>estacion1;
-
+    }
+    if(posicion1<posicion2)
+    {
+        valid=false;
+        linea1=resize(row, ':');
+        while(!valid)
+        {            
+            linea2=resize(linea1, '-');
+            linea2=grupito(linea2, '-');
+            if(linea2>=posicion1 && linea2<posicion2)
+            {
+                linea2=resize(linea1, '-');
+                linea2=resize(linea2, '-');
+                linea2=resize(linea2, '-');
+                linea2=grupito(linea2, '-');
+                tiempo+=stof(linea2);
+            }
+            linea1=resize(linea1, ',');
+            if(linea1.size()==0)
+            valid=true;
+        }
+    }
+    else if(posicion1>posicion2)
+    {
+        valid=false;
+        linea1=resize(row, ':');
+        while(!valid)
+        {            
+            linea2=resize(linea1, '-');
+            linea2=grupito(linea2, '-');
+            if(linea2<=posicion1 && linea2>posicion2)
+            {
+                linea2=resize(linea1, '-');
+                linea2=resize(linea2, '-');
+                linea2=grupito(linea2, '-');
+                tiempo+=stoi(linea2);
+            }
+            linea1=resize(linea1, ',');
+            if(linea1.size()==0)
+            valid=true;
+        }
+    }
+    return tiempo;
 }
+/*  Para imprimir el resultado de la funcion anterior:
+    int hora, min
+    time_t now=time(0);
+    tm* tActual=localtime(&now);
+    hora=tiempo/3600;
+    tiempo=tiempo%3600;
+    min=tiempo/60;
+    tiempo=tiempo%60;
+    tActual->tm_hour+=hora;
+    tActual->tm_min+=min;
+    tActual->tm_sec+=tiempo;
+    time_t newTime=mktime(tActual);
+    tm* tFinal=localtime(&newTime);
+    cout<<"La hora de llegada desde la estacion "<<estacion1<<" hasta la estacion "<<estacion2<<" es de: "<<tFinal->tm_hour<<":"<<tFinal->tm_min<<":"<<tFinal->tm_sec<<endl;*/
 
 //Linea A: berrio-2-4-6, berrio-2-4-6,
 string resize(string row, char caracter)//recorta el tamaño del string, quitando el contenido existente hasta encontrar el caracter ingresado
@@ -93,13 +174,10 @@ string grupito(string row, char caracter)
     return grupo;
 }
 
-char pedirLinea(string lineas[])
+char pedirLinea(string lineas[], int tamano)
 {
     bool valid;
     char linea;
-    int tamTotal=sizeof(lineas);
-    int tamFilas=sizeof(lineas[0]);
-    int tamano=tamTotal/tamFilas;
     while(valid)
     {
         cout<<"Ingrese la linea sobre la que desea operar"<<endl;
@@ -117,4 +195,29 @@ char pedirLinea(string lineas[])
         }
     }
     return linea;
+}
+
+bool encontrarEstacion(string row, string estacion, string* posicion)
+{
+    bool valid=true;
+    string linea2, linea3, lpos;
+    linea2=resize(row, ':');
+    while(valid)
+    {
+        linea3=grupito(linea2, '-');
+        linea3=resize(linea3, ' ');
+        if(linea3==estacion)
+        {
+            valid=false;
+            lpos=resize(linea2, '-');
+            lpos=grupito(lpos, '-');
+            *posicion=lpos;
+        }
+        linea2=resize(linea2, ',');
+        if(linea2.size()==0)
+        valid=false;
+    }
+    if(linea3==estacion)
+    valid=true;
+    return valid;//retorna true si la estacion si se encuentra en la linea
 }
